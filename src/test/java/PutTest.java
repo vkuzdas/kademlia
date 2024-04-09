3d7d493113b8916f47542e5523bed30abd32befd
vkuzdas <kuzdavoj@fel.cvut.cz>
2024-04-09 09:21:14 +0200

    testPut_nonOverlapping

--- src/test/java/PutTest.java
+++ src/test/java/PutTest.java
@@ -1,15 +1,22 @@
 import kademlia.KademliaNode;
+import kademlia.NodeReference;
 import kademlia.Util;
 import org.junit.jupiter.api.Test;
 
 import java.io.IOException;
 import java.math.BigInteger;
+import java.util.ArrayList;
 import java.util.List;
 
 import static kademlia.Util.*;
-import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.*;
 
 public class PutTest extends BaseTest {
+
+    // TODO:
+    // ~~ 1. Test put non-ovelapping regions ~~
+    // 2. Test put + join
+    // 3. Test put + leave
 
     @Test
     public void testPut_singleNode() throws IOException {
@@ -40,8 +47,59 @@ public class PutTest extends BaseTest {
 
         List<Pair> pairs = joiner.get("key1");
         assertEquals(2, pairs.size());
+    }
+
+    @Test
+    public void testPut_nonOverlapping() throws IOException {
+        List<NodeReference> lowRange = new ArrayList<>();
+        List<NodeReference> highRange = new ArrayList<>();
+
+        KademliaNode bootstrap = new KademliaNode(LOCAL_IP, BASE_PORT++, new BigInteger("2").pow(15));
+        lowRange.add(bootstrap.getNodeReference());
+        runningNodes.add(bootstrap);
+        bootstrap.initKademlia();
+
+        // low nodes
+        for (int i = 1; i <= K; i++) {
+            KademliaNode joiner = new KademliaNode(LOCAL_IP, BASE_PORT++, new BigInteger("2").pow(15).add(BigInteger.valueOf(i)));
+            joiner.join(bootstrap.getNodeReference());
+            lowRange.add(joiner.getNodeReference());
+            runningNodes.add(joiner);
+        }
+
+        // high nodes
+        for (int i = 0; i < K; i++) {
+            KademliaNode joiner = new KademliaNode(LOCAL_IP, BASE_PORT++, new BigInteger("2").pow(19).add(BigInteger.valueOf(i)));
+            joiner.join(bootstrap.getNodeReference());
+            highRange.add(joiner.getNodeReference());
+            runningNodes.add(joiner);
+        }
+
+        String lowKey = "key3"; // keyHash=885733 ∈ [2^19, 2^20)
+        String highKey = "key1"; // keyhash= 40604 ∈ [2^15, 2^16)
+
+        bootstrap.put(highKey, "highKey");
+
+        bootstrap.put(lowKey, "lowKey");
+
+        List<Pair> expectHighRange = bootstrap.get("key1");
+        List<Pair> expectLowRange = bootstrap.get("key3");
 
 
+        expectLowRange.forEach(p -> assertTrue(lowRange.contains(p.node)));
+        expectHighRange.forEach(p -> assertTrue(highRange.contains(p.node)));
+
+        for (int i = 1; i < K+1; i++) {
+            assertEquals("lowKey", runningNodes.get(i).getLocalData().get(Util.getId(lowKey)));
+            assertNull(runningNodes.get(i).getLocalData().get(Util.getId(highKey)));
+        }
+        for (int i = K+1; i < 2*K+1; i++) {
+            assertEquals("highKey", runningNodes.get(i).getLocalData().get(Util.getId(highKey)));
+            assertNull(runningNodes.get(i).getLocalData().get(Util.getId(lowKey)));
+        }
+
+        assertNull(bootstrap.getLocalData().get(Util.getId(highKey)));
+        assertNull(bootstrap.getLocalData().get(Util.getId(lowKey)));
     }
 
 }
